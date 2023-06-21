3f1cb19eee90cef66ed6b7ff5c30f9a6a07546b3
Lorcan Nicholls <72615977+lorcan2440@users.noreply.github.com>
2023-06-21 23:38:03 +0100

    Update ReadAnalogVoltage.ino

--- ReadAnalogVoltage.ino
+++ ReadAnalogVoltage.ino
@@ -1,32 +1,21 @@
-
 void setup() {
-  // initialise serial communications:
-  pinMode(NINA_RESETN, OUTPUT);         
+  // initialize serial communication at 115200 bits per second:
+  pinMode(NINA_RESETN, OUTPUT);
   digitalWrite(NINA_RESETN, LOW);
   Serial.begin(115200);
   SerialNina.begin(115200);
-  
-  int i = 0;  // measurement number (index)
-  
-  while (true) {
-    // convert sensor reading to temperature using the formula given in the datasheet (AD22100.pdf, pg. 7)
-    int sensorValue = analogRead(A0);
-    float voltage = sensorValue * (5.0 / 1023.0);
-    float temperature = (voltage - 1.375) / 0.0225;
+}
 
-    SerialNina.print("Measurement #");
-    SerialNina.print(i);
-    SerialNina.print(" - Temperature = ");
-    SerialNina.println(temperature);
-    
-    
-    if (Serial.available()) {
-      SerialNina.write(Serial.read());}
-    /*
-    if (SerialNina.available()) {
-      Serial.write(SerialNina.read());}*/  // for two-way comms - disable for now
-      
-    i += 1;
-    delay(1000);  // time delay between readings = 1000 ms = 1 s
-  };
-};
+void loop() {
+  static int i = 0;  // Static variable to retain the value of 'i' between function calls
+
+  int sensorValue = analogRead(A0);
+  byte packet[2 * sizeof(int)];
+  memcpy(packet, &sensorValue, sizeof(int));
+  memcpy(packet + sizeof(int), &i, sizeof(int));
+
+  SerialNina.write(packet, sizeof(packet));
+
+  i += 1;
+  delay(1000);
+}
